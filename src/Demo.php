2627e4df0ed913b6a9074f76b9360f62e29b6563
lizhipeng <1932769665@qq.com>
2023-02-10 15:10:42 +0800

    composer init

---
+++ src/Demo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: Lzp
+ * Date: 2023/2/10
+ * Email: <liuzp@xinlongmail.com>
+ */
+
+namespace Test\ComposerTestBar;
+
+class Demo
+{
+    public static function hello()
+    {
+        return "hello world!";
+    }
+}
